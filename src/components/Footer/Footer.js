23a9fe408790c8ebecc448f84e1c3dc47db063c0
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-05-30 23:16:51 -0400

    wip: creates starter skeleton

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+ <div>
+     <p>Coptyright © DigiMart 2021 All Rights Reserved</p>
+ </div>   
+);
+
+export default Footer
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+const Header = () => (
+    <div>
+        <nav>
+            <h2>DigiMart</h2>
+            <ul>
+                <li>Search</li>
+                <li>My Collection</li>
+                <li>Account</li>
+            </ul>
+        </nav>    
+    </div>
+)
+
+export default Header
